31fc678c0e304114801b30500c415c325f1ad4f8
lucas nico zamora jabre <lzamora@alumnos.inf.utfsm.cl>
2017-11-23 21:52:40 -0300

    Initial Commit

---
+++ TTP_HC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TTP_HC
+//
+//  Created by Lukas Zamora on 11/23/17.
+//  Copyright © 2017 Lukas Zamora. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
